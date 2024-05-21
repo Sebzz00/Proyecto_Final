7e355dbddeb5fb1768229e329956e91a140ba5df
SebasSalga2 <sebasalguz@gmail.com>
2024-05-21 17:25:15 -0500

    Commit inicial

---
+++ src/Com/Main/Main.java
@@ -0,0 +1,13 @@
+
+package Com.Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int git;
+    double donald;
+    double jesus;
+    double julian;
+    double orlando;
+    }
+}
